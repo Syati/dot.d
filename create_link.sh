9b803bde50e6674e2e1ea6a1eb0232e0a94ac5e1
mizuki-y <mizuki-y@syati.info>
2015-08-26 21:06:50 +0900

    Add create link sh

---
+++ create_link.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+CWD=`dirname $(readlink -f $0)`
+
+ln -s ${CWD}/zsh.d/.zshrc ~/.zshrc
+ln -s ${CWD}/tmux.d/.tmux.conf ~/.tmux.conf
+ln -s ${CWD}/percol.d ~/.percol.d
+ln -s ${CWD}/emacs.d ~/.emacs.d
+
