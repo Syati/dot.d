49a2dfd49462e3bcdb777c0e406f91aca5adec7f
mizuki-y <mizuki-y@syati.info>
2015-08-30 17:33:59 +0900

    Update create link

--- create_link.sh
+++ create_link.sh
@@ -6,4 +6,5 @@ ln -s ${CWD}/zsh.d/.zshrc ~/.zshrc
 ln -s ${CWD}/tmux.d/.tmux.conf ~/.tmux.conf
 ln -s ${CWD}/percol.d ~/.percol.d
 ln -s ${CWD}/emacs.d ~/.emacs.d
+ln -s ${CWD}/keysnail.d/.keysnail.js ~/.keysnail.js
 
