9dcdc0a0d2daf998ac77f516cd6821a287491706
mizuki-y <mizuki-y@syati.info>
2015-08-30 17:34:17 +0900

    Add osx settings

---
+++ osx/karabiner/karabiner-import.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.app_excel_emacs_bindings 1
+/bin/echo -n .
+$cli set remap.controlJ2return 1
+/bin/echo -n .
+$cli set option.emacsmode_controlH 1
+/bin/echo -n .
+$cli set option.emacsmode_controlAE 1
+/bin/echo -n .
+$cli set option.emacsmode_controlW 1
+/bin/echo -n .
+$cli set option.emacsmode_controlV 1
+/bin/echo -n .
+$cli set remap.app_excel_control_shift_plus 1
+/bin/echo -n .
+$cli set option.emacsmode_ex_commandW 1
+/bin/echo -n .
+$cli set private.app_pycharm_emacs_esp 1
+/bin/echo -n .
+$cli set option.emacsmode_optionV 1
+/bin/echo -n .
+$cli set option.emacsmode_controlK 1
+/bin/echo -n .
+$cli set private.app_pycharm_emacs_down_up 1
+/bin/echo -n .
+$cli set remap.undo_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.app_excel_f2_to_ctrlu 1
+/bin/echo -n .
+$cli set option.emacsmode_controlPNBF 1
+/bin/echo -n .
+$cli set option.emacsmode_controlY 1
+/bin/echo -n .
+$cli set remap.optionTab2commandTab 1
+/bin/echo -n .
+$cli set private.app_pycharm_emacs_pnbf 1
+/bin/echo -n .
+/bin/echo
