9b1bf9888b174cba5f3233b957d389ad1560eebb
Mizuki Yamamoto <m.yamamoto@raksul.com>
2016-07-17 17:35:13 +0900

    Add helm-gtags keybind

--- emacs.d/inits/30-helm-gtags.el
+++ emacs.d/inits/30-helm-gtags.el
@@ -10,4 +10,12 @@
    '(helm-gtags-path-style 'relative)
    '(helm-gtags-ignore-case t)
    '(helm-gtags-auto-update t))
+  (bind-keys :map helm-gtags-mode-map
+             ("M-t" . helm-gtags-find-tag)
+             ("M-r" . helm-gtags-find-rtag)
+             ("M-s" . helm-gtags-find-symbol)
+             ("M-g M-p" . helm-gtags-parse-file)
+             ("C-c <" . helm-gtags-previous-history)
+             ("C-c >" . helm-gtags-next-history)
+             ("M-,"  . helm-gtags-pop-stack))
   )
