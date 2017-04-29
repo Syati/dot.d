dc82e980599d31b74fe79e3c9064417c94811376
Mizuki Yamamoto <m.yamamoto@raksul.com>
2017-04-29 17:45:33 +0900

    No longer used theme

--- emacs.d/inits/90-theme.el
+++ emacs.d/inits/90-theme.el
@@ -1,6 +1,3 @@
-;; theme setting
-;;(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes"))
-
 (use-package monokai-theme
   :config (progn
             (load-theme 'monokai t)))
